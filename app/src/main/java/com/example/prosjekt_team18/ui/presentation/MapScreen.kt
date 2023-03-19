3651281e473c958c4a704928f151962ea632df2c
Jonas <jonasfn7@gmail.com>
2023-03-19 13:38:51 +0100

    Implementert Google Maps

--- app/src/main/java/com/example/prosjekt_team18/ui/presentation/MapScreen.kt
+++ app/src/main/java/com/example/prosjekt_team18/ui/presentation/MapScreen.kt
@@ -3,7 +3,6 @@ package com.example.prosjekt_team18.ui.presentation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapType
 
 @Composable
 fun MapScreen(viewModel: MapViewModel) {
