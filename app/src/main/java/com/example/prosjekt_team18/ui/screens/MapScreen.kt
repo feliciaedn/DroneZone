013a59eded5ba841d650f76d87264e3b13749bfb
Jonas <jonasfn7@gmail.com>
2023-03-30 17:24:24 +0200

    Implementert mer WIP på GPS-disabled knapp

--- app/src/main/java/com/example/prosjekt_team18/ui/screens/MapScreen.kt
+++ app/src/main/java/com/example/prosjekt_team18/ui/screens/MapScreen.kt
@@ -44,6 +44,7 @@ fun MapScreen(mapViewModel: MapViewModel, cameraPositionState: CameraPositionSta
 				properties = positionUiState.properties,
 				cameraPositionState = cameraPositionState
 			)
+
 			if (userLocation == LatLng(0.0, 0.0)) {
 				IconButton(
 					modifier = Modifier
